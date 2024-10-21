58452a24279a5ea445269ed28fbec58db5836d78
QuocChung <tuquocchung@gmail.com>
2024-10-21 17:39:25 +0700

    THEM PHAN SUA VA XOA

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
-using _1._alazea_gh_pages.Models;
+﻿using _1._alazea_gh_pages.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
 
 namespace _1._alazea_gh_pages.Controllers
@@ -22,10 +23,74 @@ namespace _1._alazea_gh_pages.Controllers
             return View(list_Sach);
         }
 
-        public IActionResult Privacy()
+        [Route("ThemSachMoi")]
+        [HttpGet]
+      
+        public IActionResult ThemSachMoi()
         {
+            ViewBag.MaLoai = new SelectList(db.TLoaiSaches.ToList(), "MaLoai", "TenLoai");
+            ViewBag.MaNgonNgu = new SelectList(db.TNgonNgus.ToList(), "MaNgonNgu", "TenNgonNgu");
+            ViewBag.MaNxb = new SelectList(db.TNhaXbs.ToList(), "MaNxb", "TenNxb");
+            var list_Sach = db.TSaches.ToList();
+            ViewBag.TenNgonNgu = db.TNgonNgus.ToList();
             return View();
         }
-     
+
+        [Route("ThemSachMoi")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ThemSachMoi(TSach Sach)
+        {
+            /*- == true thì chứng tỏ dứ liệu truyền từ form vào hợp lệ -*/
+            if (ModelState.IsValid)
+            {
+                db.Add(Sach);
+                db.SaveChanges();
+                return RedirectToAction("Index"); // Chuyển hướng về trang danh sách
+            }
+            var list_Sach = db.TSaches.ToList();
+            ViewBag.TenNgonNgu = db.TNgonNgus.ToList();
+            return View();
+        }
+
+
+
+
+
+
+        /*- Xóa sách thì chúng ta sẽ phải cần mã sách -*/
+        [Route("XoaSach")]
+        [HttpGet]
+        public IActionResult XoaSach(string MaSach)
+        {
+            TempData["Message"] = "";
+            var list_Sach = db.TSaches.ToList();
+            ViewBag.TenNgonNgu = db.TNgonNgus.ToList();
+            // Kiểm tra chi tiết sản phẩm trước khi xóa
+            var chiTietSachTonTai = db.TBanSaoSaches.Any(x => x.MaSach == MaSach);
+            if (chiTietSachTonTai)
+            {
+                TempData["Message"] = "Không xóa được sản phẩm này vì có chi tiết sản phẩm liên quan.";
+                return RedirectToAction("Index", "Home");
+            }
+
+            // Xóa sản phẩm trong bảng TDanhMucSps
+            var Sach = db.TSaches.Find(MaSach);
+            if (Sach != null)
+            {
+                db.Remove(Sach);
+                db.SaveChanges();
+                TempData["Message"] = "Sản phẩm đã được xóa.";
+            }
+            else
+            {
+                TempData["Message"] = "Sản phẩm không tồn tại.";
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
     }
 }
